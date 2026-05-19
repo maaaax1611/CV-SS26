d61095b8ec4aad75e0caab00b2e390e7469ce9ba
Max <max.mm.mueller@siemens-healthineers.com>
2026-05-19 17:16:07 +0200

    Implement harris response computation

--- feature_detection/utils/functions.py
+++ feature_detection/utils/functions.py
@@ -17,19 +17,32 @@ def compute_harris_response(I: np.array, k: float = 0.06) -> Tuple[np.array]:
     assert I.dtype == np.float32
 
     # Step 1: Compute Idx and Idy with cv2.Sobel
-
+    Idx = cv2.Sobel(I, cv2.CV_32F, 1, 0, ksize=3)
+    Idy = cv2.Sobel(I, cv2.CV_32F, 0, 1, ksize=3)
 
     # Step 2: Ixx Iyy Ixy from Idx and Idy
-
+    Ixx = Idx * Idx
+    Iyy = Idy * Idy
+    Ixy = Idx * Idy
 
     # Step 3: compute A, B, C from Ixx, Iyy, Ixy with cv2.GaussianBlur
     # Use sdev = 1 and kernelSize = (3, 3) in cv2.GaussianBlur
-
+    # vs2.GaussianBlur(image, kernelSize, sdev)
+    A = cv2.GaussianBlur(src=Ixx, ksize=(3, 3), sigmaX=1)
+    B = cv2.GaussianBlur(src=Iyy, ksize=(3, 3), sigmaX=1)
+    C = cv2.GaussianBlur(src=Ixy, ksize=(3, 3), sigmaX=1)
 
     # Step 4: Compute the harris response with the determinant and the trace of T
+    T = np.array(
+        [[A, C],
+        [C, B]]
+        )
+    det = np.linalg.det(T.transpose(2, 3, 0, 1))
+    trace = A + B
+    trace = np.trace(T.transpose(2, 3, 0, 1), axis1=2, axis2=3)
+    R = det - k * trace**2
 
-
-    raise NotImplementedError
+    return R, A, B, C, Idx, Idy
 
 
 def detect_corners(R: np.array, threshold: float = 0.1) -> Tuple[np.array, np.array]:
